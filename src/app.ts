d16cbe0d228cd3b155b14a7dcb8160241b7b51b2
Jon Hogendorp <jonhoog@live.nl>
2024-06-12 16:59:35 +0200

    updates

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,23 @@
 // Code goes here!
+//autobind decorator
+function autobind(
+        _: any,
+        _2: string, 
+        descriptor: PropertyDescriptor){
+        const originalMethod = descriptor.value;
+        const adjDescriptor: PropertyDescriptor ={
+                configurable: true,
+                get(){
+                        const boundfn = originalMethod.bind(this);
+                        return boundfn;
+                }
+        
+        };
+        return adjDescriptor;
+}
+
+
+//Project Input Class
 
 class projectinput{
         templateElement: HTMLTemplateElement;
@@ -24,9 +43,39 @@ class projectinput{
                 this.attach();
         }
 
+        private gatherUserInput(): [string, string, number] | void{
+                const enteredTitle = this.titleInputElement.value;
+                const enteredDescription = this.descriptionInputElement.value;
+                const enteredPeople = this.peopleInputElement.value;
+
+                if(
+                        enteredTitle.trim().length === 0 ||
+                        enteredDescription.trim().length === 0 ||
+                        enteredPeople.trim().length === 0
+                ){
+                        alert('Invalid input, please try again');
+                        return;
+                        }else{
+                                return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+        }
+
+        private clearInputs(){
+                this.titleInputElement.value = '';
+                this.descriptionInputElement.value = '';
+                this.peopleInputElement.value = '';
+        }
+
+
+        @autobind
         private submitHandler(event: Event){
                 event.preventDefault();
-                console.log(this.titleInputElement.value);
+                const userInput = this.gatherUserInput();
+                if(Array.isArray(userInput)){
+                        const [title, desc, people] = userInput;
+                        console.log(title, desc, people);
+                        this.clearInputs();
+                }
         }
 
         private configure(){
